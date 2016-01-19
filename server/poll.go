ecf9b496660fd3d7dc9ed9ce579484e68dc620f5
Lyon Hill <lyondhill@gmail.com>
2016-01-19 10:19:00 -0700

    modify the way the server works to be a singleton server
    this change makes it more clear that the server is the primary
    functionality of the pulse system and you shouldnt be creating
    multiple servers.

--- server/poll.go
+++ server/poll.go
@@ -8,18 +8,18 @@ import (
 // StartPolling(nil, nil, 60, ch)
 // StartPolling(nil, []string{"cpu"}, 1, ch)
 // StartPolling([]string{"computer1", "computer2"}, []string{"cpu"}, 1, ch)
-func (server *Server) StartPolling(ids, tags []string, interval time.Duration, done chan struct{}) {
+func StartPolling(ids, tags []string, interval time.Duration, done chan struct{}) {
 	tick := time.Tick(interval)
 	for {
 		select {
 			case <-tick:
 				if ids != nil {
-					server.Poll(tags)
+					Poll(tags)
 					continue
 				}
 
 				newIds := []string{}
-				for _, sid := range server.findIds(tags) {
+				for _, sid := range findIds(tags) {
 					for _, id := range ids {
 						if id == sid {
 							newIds = append(newIds, id)
@@ -27,30 +27,26 @@ func (server *Server) StartPolling(ids, tags []string, interval time.Duration, d
 					}
 				}
 				command := "get " + strings.Join(tags, ",") + "\n"
-				server.sendAll(command, ids)
+				sendAll(command, ids)
 			case <-done:
 				return
 		}
 	}
 }
 
-func (server *Server) Poll(tags []string) {
+func Poll(tags []string) {
 	if tags == nil {
-		server.PollAll()
+		PollAll()
 		return
 	}
-	ids := server.findIds(tags)
+	ids := findIds(tags)
 	command := "get " + strings.Join(tags, ",") + "\n"
-	server.sendAll(command, ids)
+	sendAll(command, ids)
 }
 
-func (server *Server) PollAll() {
-	for id, conn := range server.connections {
-		tags := []string{}
-		for tag, _ := range server.mappings[id] {
-			tags = append(tags, tag)
-		}
-		command := "get " + strings.Join(tags, ",") + "\n"
-		go conn.Write([]byte(command))
+func PollAll() {
+	for _, client := range clients {
+		command := "get " + strings.Join(client.tags, ",") + "\n"
+		go client.conn.Write([]byte(command))
 	}	
 }
