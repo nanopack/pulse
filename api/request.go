29044cc9e70f4da98db6c0d014470af2a7a82317
Greg Linton <lintong@pagodabox.com>
2016-08-23 13:38:26 -0600

    Make daily stat times sorted. Allow option to backfill data

--- api/request.go
+++ api/request.go
@@ -162,7 +162,7 @@ func hourlyStat(res http.ResponseWriter, req *http.Request) {
 
 	filters := []string{}
 	for key, val := range req.URL.Query() {
-		if key == ":stat" || key == "verb" || key == "start" || key == "stop" {
+		if key == ":stat" || key == "verb" || key == "start" || key == "stop" || key == "backfill" || key == "x-auth-token" || key == "X-AUTH-TOKEN" {
 			continue
 		}
 		// if there are multiple values, "OR" them, and save as one filter element
@@ -187,8 +187,15 @@ func hourlyStat(res http.ResponseWriter, req *http.Request) {
 		query = fmt.Sprintf("%s WHERE %s", query, strings.Join(filters, " AND "))
 	}
 
-	// grab the hourly averages ('group by' should aggregate multiple filters)
-	query = fmt.Sprintf("%s GROUP BY time(1h) fill(none)", query) //  OR fill(0) to not return 0 for empty values
+	bf := req.URL.Query().Get("backfill")
+
+	if bf == "" {
+		// grab the hourly averages ('group by' should aggregate multiple filters)
+		query = fmt.Sprintf("%s GROUP BY time(1h) fill(none)", query) //  OR fill(0) to not return 0 for empty values
+	} else {
+		// grab the hourly averages ('group by' should aggregate multiple filters)
+		query = fmt.Sprintf("%s GROUP BY time(1h) fill(0)", query) //  OR fill(0) to not return 0 for empty values
+	}
 
 	records, err := influx.Query(query)
 	if err != nil {
@@ -239,7 +246,7 @@ func dailyStat(res http.ResponseWriter, req *http.Request) {
 
 	filters := []string{}
 	for key, val := range req.URL.Query() {
-		if key == ":stat" || key == "verb" || key == "start" || key == "stop" {
+		if key == ":stat" || key == "verb" || key == "start" || key == "stop" || key == "backfill" || key == "x-auth-token" || key == "X-AUTH-TOKEN" {
 			continue
 		}
 		// if there are multiple values, "OR" them, and save as one filter element
@@ -285,12 +292,25 @@ func dailyStat(res http.ResponseWriter, req *http.Request) {
 		valUnix := time.Unix(valTime, 0)
 		hour, minute, _ := valUnix.Clock()
 		minute = int(math.Floor(float64(minute/15.0)) * 15)
-		id := fmt.Sprintf("%v:%v", hour, minute)
+		id := fmt.Sprintf("%02d:%02d", hour, minute)
 		valData, _ := values[1].(json.Number).Float64()
 
 		// keep a running average instead of a total
 		result[id] = ((result[id] * float64(counter[id])) + valData) / float64(counter[id]+1)
 		counter[id] += 1
+	}
+
+	bf := req.URL.Query().Get("backfill")
+
+	// if backfill set to anything, fill in the missing data with 0 value
+	if bf != "" {
+		daytimes := []string{"00:00", "00:15", "00:30", "00:45", "01:00", "01:15", "01:30", "01:45", "02:00", "02:15", "02:30", "02:45", "03:00", "03:15", "03:30", "03:45", "04:00", "04:15", "04:30", "04:45", "05:00", "05:15", "05:30", "05:45", "06:00", "06:15", "06:30", "06:45", "07:00", "07:15", "07:30", "07:45", "08:00", "08:15", "08:30", "08:45", "09:00", "09:15", "09:30", "09:45", "10:00", "10:15", "10:30", "10:45", "11:00", "11:15", "11:30", "11:45", "12:00", "12:15", "12:30", "12:45", "13:00", "13:15", "13:30", "13:45", "14:00", "14:15", "14:30", "14:45", "15:00", "15:15", "15:30", "15:45", "16:00", "16:15", "16:30", "16:45", "17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00", "20:15", "20:30", "20:45", "21:00", "21:15", "21:30", "21:45", "22:00", "22:15", "22:30", "22:45", "23:00", "23:15", "23:30", "23:45"}
+
+		for i := range daytimes {
+			if _, ok := result[daytimes[i]]; !ok {
+				result[daytimes[i]] = 0.0
+			}
+		}
 	}
 
 	writeBody(result, res, http.StatusOK, req)
