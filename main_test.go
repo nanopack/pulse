881c8e1e8c2f7ffac838ce6f5b20b1fc8760f7fc
Greg Linton <lintong@pagodabox.com>
2016-12-16 16:44:24 -0700

    Add output to see where travis hangs

--- main_test.go
+++ main_test.go
@@ -24,6 +24,7 @@ func TestMain(m *testing.M) {
 		wait.Add(-len(msgSet.Messages))
 		return nil
 	})
+	fmt.Println("Server listening...")
 
 	if err != nil {
 		panic(fmt.Sprintf("unable to listen %v", err))
@@ -33,29 +34,42 @@ func TestMain(m *testing.M) {
 }
 
 func TestEndToEnd(test *testing.T) {
+	fmt.Println("Testing end to end...")
 	relay, err := relay.NewRelay(address, "relay.station.1")
 	if err != nil {
 		test.Errorf("unable to connect to server %v", err)
 		return
 	}
 	defer relay.Close()
+	fmt.Println("New relay created...")
 
 	relay.AddCollector("cpu", []string{"hi", "how", "are:you"}, randCollector())
+	fmt.Println("CPU relay added...")
 
 	relay.AddCollector("ram", nil, randCollector())
+	fmt.Println("RAM relay added...")
 
 	relay.AddCollector("disk", nil, randCollector())
+	fmt.Println("DISK relay added...")
+
 	time.Sleep(time.Millisecond * 100)
+
+	fmt.Println("DISK polling...")
 	wait.Add(1)
 	server.Poll([]string{"disk"})
+	fmt.Println("DISK polled")
 	wait.Wait()
 
+	fmt.Println("RAM, CPU polling...")
 	wait.Add(2)
 	server.Poll([]string{"ram", "cpu"})
+	fmt.Println("RAM, CPU polled")
 	wait.Wait()
 
+	fmt.Println("ALL polling...")
 	wait.Add(3)
 	server.Poll([]string{"ram", "cpu", "disk"})
+	fmt.Println("ALL polled")
 	wait.Wait()
 
 	if len(messages) != 3 {
