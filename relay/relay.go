6876cadc60d11a5fff5d7b89c58d0c5ae5189753
Greg Linton <lintong@pagodabox.com>
2018-04-11 10:36:50 -0600

    Allow more docker container names

--- relay/relay.go
+++ relay/relay.go
@@ -249,7 +249,7 @@ func (relay *Relay) Info() map[string]float64 {
 
 // AddCollector adds a collector to relay
 func (relay *Relay) AddCollector(name string, tags []string, collector Collector) error {
-	if name == "_connected" || strings.ContainsAny(name, "-:,") {
+	if name == "_connected" || strings.ContainsAny(name, ":,") {
 		lumber.Trace("[PULSE :: RELAY] Reserved name!")
 		return ReservedName
 	}
